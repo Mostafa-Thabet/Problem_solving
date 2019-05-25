5bc69d0278aaaaee572122846188f28967c04c23
mosta <mosta@DESKTOP-69QBRLA>
2019-05-25 02:00:50 +0200

    # WARNING: head commit changed in the meantime
    [interviewbit] kth Row of Pascal's

---
+++ InterviewBit/Kth Row of Pascal's Triangle.cpp
@@ -0,0 +1,18 @@
+/*
+ * Kth Row of Pascal's Triangle.cpp
+ *
+ *  Created on: May 24, 2019
+ *      Author: mosta
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!"
+			"ello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
+
+
